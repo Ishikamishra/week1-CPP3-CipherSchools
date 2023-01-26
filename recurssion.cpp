fca3fbc2a746239f6dd856ac5354462b631ec393
Ishikamishra <81811063+Ishikamishra@users.noreply.github.com>
2023-01-26 13:09:59 +0530

    Create recurssion.cpp

---
+++ recurssion.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+ using namespace std;
+ //iterative void 
+ //int power(int base,int pow) {
+ //int num =int16_t// for(int . i = 0; i<pow; i++){
+     // num*=base; //num = num*base;
+// }
+// return num;
+// }
+
+
+// recursive fun 
+int power(int base, int pow){
+    if (pow == 0)return 1;
+    int smlAns = power(base, pow-1);
+    return smlAns*base;
+}
+int main(){
+ cout<<power(2,3);
+ 
+ return 0;
+}
